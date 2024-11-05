ac353d3776b6a45a8077368918e9f8f72d68fd41
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2024-11-06 00:19:00 +0530

    feat: add proper question api routes

--- questions/create.go
+++ questions/create.go
@@ -1,17 +1,33 @@
 package questions
 
 import (
+	"github.com/delta18-git/pesuio-final-project/database"
 	"github.com/delta18-git/pesuio-final-project/models"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateQuestion(c *gin.Context) {
 	var request models.CreateQuestionRequest
-	c.BindJSON(&request)
-
-	c.JSON(200, gin.H{
-		"success":    true,
-		"questionID": 1,
-	})
-
+	err := c.BindJSON(&request)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "invalid input",
+		})
+	}
+	question := models.Question{
+		Question:  request.Question,
+		TestCases: request.TestCases,
+		Score:     request.Score,
+	}
+	result := database.DB.Create(&question)
+	if result.Error != nil {
+		c.JSON(400, gin.H{
+			"error": "unable to create question",
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"success":    true,
+			"questionID": question.ID,
+		})
+	}
 }
--- questions/fetch.go
+++ questions/fetch.go
@@ -1,6 +1,7 @@
 package questions
 
 import (
+	"github.com/delta18-git/pesuio-final-project/database"
 	"github.com/delta18-git/pesuio-final-project/models"
 	"github.com/gin-gonic/gin"
 )
@@ -10,9 +11,25 @@ func FetchQuestion(c *gin.Context) {
 	c.BindJSON(&request)
 
 	var question models.Question
-	c.JSON(200, gin.H{
-		"success":  true,
-		"question": question,
-	})
+	result := database.DB.Where("ID = ?", request.QuestionID).First(&question)
+	if result.Error != nil {
+		c.JSON(400, gin.H{
+			"error": "question not found",
+		})
+	} else {
+		var testCases []models.TestCase
+		err := database.DB.Model(&question).Association("TestCases").Find(&testCases)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "error fetching test cases",
+			})
+			return
+		}
+		question.TestCases = testCases
+		c.JSON(200, gin.H{
+			"success":  true,
+			"question": question,
+		})
+	}
 
 }
