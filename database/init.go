03c3959b39d4b2bf3579ed2ef1509c148a8a9ff3
nims <nimalanke24@gmail.com>
2024-11-05 21:38:58 +0530

    added code for signin request

--- database/init.go
+++ database/init.go
@@ -5,7 +5,11 @@ import "gorm.io/gorm"
 var DB *gorm.DB
 
 func Init(databaseFileName string) {
+	DB, err = gorm.Open(sqlite.Open("backend.db"), &gorm.Config{})
+	if err != nil {
+		panic("Error connecting to database")
+	}
 	// implement
-	// populate DB variable
+	// populate DB variable	
 
 }
